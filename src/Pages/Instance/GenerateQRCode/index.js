b30afd2b52eb4eef8161f5f2198749c1efa3fbb9
Omar Bahmad <78921702+OmarBahmad@users.noreply.github.com>
2021-08-10 16:04:35 -0300

    GenerateQRCode

---
+++ src/Pages/Instance/GenerateQRCode/index.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import { getQRCode } from "../../../Actions/Instances";
+
+export default function QRCode() {
+    const [webhook, setWebhook] = useState("")
+
+  async function getQR() {
+    const obj = {
+        webhook
+    }
+    try {
+    await getQRCode(obj);
+    alert("Webhook Enviado")
+      const resp = await getQRCode();
+      console.log(resp);
+    } catch (err) {
+      console.log("erro");
+    }
+  }
+
+  return (
+    <div>
+      <h2>Gerar novo QRCode</h2>
+      <input
+        placeholder="Gerar novo QR Code"
+        className="input-msg"
+        value={webhook}
+        onChange={(e) => setWebhook(e.target.value)}
+      />
+      <button onClick={getQR}>Gerar nvo QRCode</button>
+    </div>
+  );
+}
+
--- src/Pages/Instance/index.js
+++ src/Pages/Instance/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import RefreshWebhook from "./RefreshWebhook";
 import Status from "./Status";
 import UpdateProfile from "./UpdateProfile";
+import QRCode from "./GenerateQRCode"
 
 export default function Instance() {
   return (
     <div>
+      <QRCode />
       <Status />
       <UpdateProfile />
       <RefreshWebhook />
