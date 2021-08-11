8f546568028382938b5ab6b7086aea12ee1e07c3
vitorhenrique018 <vitorhenrique018@gmail.com>
2021-08-11 16:14:01 -0300

    excell Finished

--- src/Pages/Message/SendMessage/index.js
+++ src/Pages/Message/SendMessage/index.js
@@ -1,9 +1,41 @@
 import React, { useState } from "react";
+import * as XLSX from "xlsx";
 import { sendMessage } from "../../../Actions/Message";
+//import ImportExcell from "../../../Components/ImportExcell";
 
 export default function SendMessage() {
   const [message, setMessage] = useState("");
   const [number, setNumber] = useState("");
+  const [items, setItems] = useState([]);
+
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      setItems(d);
+    });
+  };
 
   async function sendM() {
     const obj = {
@@ -19,6 +51,7 @@ export default function SendMessage() {
     setMessage("");
     setNumber("");
   }
+
   return (
     <div>
       <h2>Enviar Mensagem</h2>
@@ -35,6 +68,17 @@ export default function SendMessage() {
         onChange={(e) => setMessage(e.target.value)}
       />
       <button onClick={sendM}>Enviar Mensagem</button>
+
+      <input
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}
+      />
+      {items?.map((index) => (
+        <p>{index.Nome}</p>
+      ))}
     </div>
   );
 }
