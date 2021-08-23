473c3e388892cc1c83ef5553bfb7c273ff75b311
Omar Bahmad <78921702+OmarBahmad@users.noreply.github.com>
2021-08-23 17:37:28 -0300

    AuthorizationKey

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -4,31 +4,38 @@ import { NavLink, Navigate, Route, useNavigate } from "react-router-dom";
 export default function Login() {
   const [chatId, setChatId] = useState("");
   const [tokenID, setToken] = useState("");
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   function handleLogin() {
+    setChatId("");
+    setToken("");
+    if (
+      !(
+        chatId.includes("chatpro") &&
+        chatId.length >= 18 &&
+        tokenID.length >= 29
+      )
+    )
+      return alert("ChatID ou token invalido");
     localStorage.setItem("@chatID", JSON.stringify(chatId));
     localStorage.setItem("@tokenID", JSON.stringify(tokenID));
-    navigate("/message")
-
+    navigate("/message");
   }
   return (
     <div>
       <input
         type="text"
+        value={chatId}
         placeholder="Informar o ChatID"
         onChange={(e) => setChatId(e.target.value)}
       />
       <input
         type="text"
+        value={tokenID}
         placeholder="Informar o Token"
         onChange={(e) => setToken(e.target.value)}
       />
-        <button onClick={handleLogin}>
-         Login 
-        </button>
-
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
