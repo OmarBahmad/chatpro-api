47120126ce701f8a3964ab29abe55dcbb926ad67
vitorhenrique018 <vitorhenrique018@gmail.com>
2021-09-08 16:44:34 -0300

    ajusts ok

--- src/Pages/Message/SendContact/index.js
+++ src/Pages/Message/SendContact/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Sidebar from "../../../Components/newSidebar/Sidebar";
+import DataTable from "../../../Components/DataTable";
+import * as S from "./Styles";
 
 import SendUniqueContact from "./SendUniqueContact";
 import SendMultipleContact from "./SendMultipleContact";
@@ -13,19 +15,23 @@ export default function SendMessage() {
   return (
     <>
       <Sidebar />
-      <h2>Enviar contatos</h2>
-
-      <SendUniqueContact />
-      <SendMultipleContact
-        items={items}
-        setItems={setItems}
-        setCheckMessage={setCheckMessage}
-        checkMessage={checkMessage}
-        respTrue={respTrue}
-        setRespTrue={setRespTrue}
-        respFalse={respFalse}
-        setRespFalse={setRespFalse}
-      />
+      <S.Container>
+        <S.Column1>
+          <h2>Enviar contatos</h2>
+          <SendUniqueContact />
+          <SendMultipleContact
+            items={items}
+            setItems={setItems}
+            setCheckMessage={setCheckMessage}
+            checkMessage={checkMessage}
+            respTrue={respTrue}
+            setRespTrue={setRespTrue}
+            respFalse={respFalse}
+            setRespFalse={setRespFalse}
+          />
+        </S.Column1>
+        <S.Column2>sd</S.Column2>
+      </S.Container>
     </>
   );
 }
--- src/Pages/Message/SendFile/SendUniqueFile/index.js
+++ src/Pages/Message/SendFile/SendUniqueFile/index.js
@@ -58,9 +58,3 @@ export function SendUniqueFile() {
     </S.Container>
   );
 }
-
-/*
-      caption: "Tcc Vitor",
-      number: "62982536654",
-      url: "http://repositorio.unicamp.br/bitstream/REPOSIP/254842/1/Oliveira_MiguelMeirellesde_M.pdf",
-*/
