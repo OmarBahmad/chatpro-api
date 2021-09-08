836786069df0326b30aa98546d4ad97d3b1c8624
vitorhenrique018 <vitorhenrique018@gmail.com>
2021-09-08 10:08:56 -0300

    new button import excel5

---
+++ src/Components/ImportExcel/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+export const InputFile = styled.input`
+ display: none;
+
+`;
+
+export const LabelFile = styled.label`
+  font-weight: 500;
+  color: rgb(255, 255, 255);
+  outline: none;
+  border-radius: 0.4rem;
+  padding: 0.8rem 1.5rem;
+  transition: all 0.2s ease 0s;
+  cursor: pointer;
+  background: #27251f;
+  border: 1px solid #27251f;
+`;
