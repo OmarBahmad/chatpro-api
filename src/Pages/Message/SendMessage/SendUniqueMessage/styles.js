5e29a2ddba10ea41f6aef0f044d69d106ca028db
Omar Bahmad <78921702+OmarBahmad@users.noreply.github.com>
2021-09-10 16:41:03 -0300

    Send Message Layout

--- src/Pages/Message/SendMessage/SendUniqueMessage/styles.js
+++ src/Pages/Message/SendMessage/SendUniqueMessage/styles.js
@@ -3,10 +3,20 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   margin-top: 10px;
+  margin-left: 50px;
+  background: #1d1d26;
 `;
 export const ContainerHeader = styled.div`
+  font-family: Poppins;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 36px;
+  letter-spacing: 0em;
+  text-align: left;
   width: 100%;
-  margin-bottom: 5px;
+  margin: 1rem 1.5rem 0.5rem 1.5rem;
+  padding-top: 1.5rem;
 `;
 
 export const ContainerForm = styled.div`
@@ -17,36 +27,53 @@ export const ContainerForm = styled.div`
 `;
 
 export const InputNumber = styled.input`
-  width: 80%;
+  background: #15151c;
+  font-family: "Poppins", sans-serif;
+  width: 85%;
   height: 2.25rem;
   border-radius: 5px;
-  padding: 0.5rem 1rem;
+  padding: 1.5rem 1rem;
+  margin: 1rem 1.5rem;
   border: 1px solid black;
   outline: none;
   font-size: 18px;
+  color: #fff;
+  ::placeholder {
+    color: #5e5e5e;
+  }
 `;
 
 export const InputMessage = styled.input`
-  width: 80%;
+  background: #15151c;
+  font-family: "Poppins", sans-serif;
+  width: 85%;
   height: 2.25rem;
-  padding: 0.5rem 1rem;
+  padding: 1.5rem 1rem;
+  margin: 1rem 1.5rem;
   border-radius: 5px;
   border: 1px solid black;
   outline: none;
   font-size: 18px;
   margin-top: 10px;
+  color: #fff;
+  ::placeholder {
+    color: #5e5e5e;
+  }
 `;
 
 export const ButtonSend = styled.button`
   font-weight: 500;
+  font-family: "Poppins", sans-serif;
+  font-size: 1rem;
   color: rgb(255, 255, 255);
   outline: none;
   border-radius: 0.4rem;
-  width: 50%;
-  height: 2.5rem;
+  width: 85%;
+  padding: 0.8rem 1rem;
+  margin: 1rem 1.5rem 1.5rem 1.5rem;
   transition: all 0.2s ease 0s;
   cursor: pointer;
-  background: rgb(4, 170, 109);
+  background: #179788;
   border: 1px solid rgb(4, 170, 109);
   margin-top: 10px;
 
--- src/Pages/Message/SendMessage/Styles.js
+++ src/Pages/Message/SendMessage/Styles.js
@@ -1,16 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+color: #fff;
   width: 100%;
+  height: 100vh;
   display: flex;
   padding-left: 0.5rem;
+  background: #15151C;
   @media (max-width: 768px) {
     flex-direction: column;
   }
 `;
 
 export const Column1 = styled.div`
-  width: 35%;
+  width: 40%;
   flex-direction: column;
   @media (max-width: 768px) {
     width: 100%;
@@ -20,7 +23,7 @@ export const Column1 = styled.div`
 export const Column2 = styled.div`
   display: flex;
   justify-content: center;
-  width: 65%;
+  width: 60%;
   @media (max-width: 768px) {
     width: 100%;
   }
